19932c489445a90364e1a35ea38e53e057190e90
Bogdan Kolesnyk <jinnzest@gmail.com>
2017-04-28 20:05:56 +0200

    now monad loger is used instead of global logger

--- logger/Main.hs
+++ logger/Main.hs
@@ -1,37 +1,18 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
-module Main
-( main
-) where
+import Control.Monad.IO.Class ( MonadIO(liftIO) )
+import Control.Monad.Logger ( runFileLoggingT )
+import Control.Monad.Logger.JSON ( logInfoJ, logDebugJ )
+import Data.Aeson.TH ( defaultOptions, deriveJSON )
+import Data.Time.Clock ( UTCTime, getCurrentTime )
 
-import System.Log.FastLogger
-import qualified Data.ByteString.Char8 as BS
-import System.IO.Unsafe
-import Data.Configurator
-import Data.Configurator.Types
-import Data.Time.Clock
-import Data.Time.ISO8601
+data Message = Message { time :: UTCTime }
 
-data LogLevel = Debug | Info | Warning | Error
+$( deriveJSON defaultOptions ''Message )
 
-instance Show LogLevel where
-  show l = case l of
-    Debug -> "[DEBUG]"
-    Info -> "[INFO]"
-    Warning -> "[WARN]"
-    Error -> "[ERROR]"
-
-main = writeLog "test log" Debug
-
-{-# NOINLINE globalLogger #-}
-globalLogger :: LoggerSet
-globalLogger =
-  unsafePerformIO $ newFileLoggerSet defaultBufSize "./info.log"
-
-writeLog :: BS.ByteString -> LogLevel -> IO ()
-writeLog str logLevel = do
-  utcTime <- getCurrentTime
-  let timestampStr = formatISO8601Millis utcTime
-      timestamp    = BS.pack timestampStr
-      logEntry     = timestamp `BS.append` (BS.pack " ") `BS.append` (BS.pack $ show logLevel) `BS.append` (BS.pack " - ") `BS.append` str `BS.append` (BS.pack "\n")
-  pushLogStr globalLogger $ toLogStr logEntry
+main :: IO ()
+main =
+  runFileLoggingT "info.log"
+    (do now <- liftIO getCurrentTime
+        $logDebugJ (Message now)
+        $logInfoJ "Hello world")
